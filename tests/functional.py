509495313bf002ace7047aebba69ad08157b37a5
Remi Rampin <remirampin@gmail.com>
2015-08-07 16:00:55 -0400

    Make tests verbose

--- tests/functional.py
+++ tests/functional.py
@@ -46,6 +46,8 @@ def functional_tests():
     destination = os.environ['TEJ_DESTINATION']
     logging.info("Using TEJ_DESTINATION %s" % destination)
 
+    tej = ['tej', '-v', '-v']
+
     for path in ('~/.tej', '~/tej2'):
         path = Path(path).expand_user()
         try:
@@ -55,33 +57,33 @@ def functional_tests():
     Path('~/tej2').expand_user().mkdir()
 
     logging.info("Creating default queue")
-    check_call(['tej', 'setup', destination])
+    check_call(tej + ['setup', destination])
     assert Path('~/.tej').expand_user().is_dir()
     Path('~/.tej').expand_user().rmtree()
 
     logging.info("Creating a queue with a link")
-    check_call(['tej', 'setup', destination,
-                '--queue', '~/tej2/queue',
-                '--make-link', '~/tej2/link'])
+    check_call(tej + ['setup', destination,
+                      '--queue', '~/tej2/queue',
+                      '--make-link', '~/tej2/link'])
     assert Path('~/tej2/queue').expand_user().is_dir()
     with Path('~/tej2/link').expand_user().open('r') as fp:
         assert fp.read() == 'tejdir: %s\n' % Path('~/tej2/queue').expand_user()
 
     logging.info("Adding links")
-    check_call(['tej', 'setup', destination, '--only-links',
-                '--queue', '~/tej2/queue',
-                '--make-link', '~/tej2/link2'])
+    check_call(tej + ['setup', destination, '--only-links',
+                      '--queue', '~/tej2/queue',
+                      '--make-link', '~/tej2/link2'])
     with Path('~/tej2/link2').expand_user().open('r') as fp:
         assert fp.read() == 'tejdir: %s\n' % Path('~/tej2/queue').expand_user()
     assert not Path('~/.tej').expand_user().exists()
-    check_call(['tej', 'setup', destination, '--only-links',
-                '--queue', '~/tej2/queue',
-                '--make-default-link'])
+    check_call(tej + ['setup', destination, '--only-links',
+                      '--queue', '~/tej2/queue',
+                      '--make-default-link'])
     with Path('~/.tej').expand_user().open('r') as fp:
         assert fp.read() == 'tejdir: %s\n' % Path('~/tej2/queue').expand_user()
 
     logging.info("Calling status for non-existent job")
-    output = check_output(['tej', 'status', destination, '--id', 'nonexistent'])
+    output = check_output(tej + ['status', destination, '--id', 'nonexistent'])
     assert output == b'not found\n'
 
     logging.info("Submitting a job")
@@ -100,13 +102,13 @@ def functional_tests():
             fp.write(b'data1\n')
         with jobdir.mkdir('dir2').mkdir('dir3').open('w', 'data2') as fp:
             fp.write('data2\n')
-        job_id = check_output(['tej', 'submit', destination, jobdir.path])
+        job_id = check_output(tej + ['submit', destination, jobdir.path])
         job_id = job_id.rstrip().decode('ascii')
     finally:
         jobdir.rmtree()
 
     logging.info("Check status of running job")
-    output = check_output(['tej', 'status', destination, '--id', job_id])
+    output = check_output(tej + ['status', destination, '--id', job_id])
     assert output == b'running\n'
 
     logging.info("Finish job")
@@ -114,14 +116,14 @@ def functional_tests():
     time.sleep(2)
 
     logging.info("Check status of finished job")
-    output = check_output(['tej', 'status', destination, '--id', job_id])
+    output = check_output(tej + ['status', destination, '--id', job_id])
     assert output == b'finished 0\n'
 
     logging.info("Download job results")
     destdir = Path.tempdir(prefix='tej-tests-')
     try:
-        check_call(['tej', 'download', destination, '--id', job_id,
-                    'job1results', '_stdout'],
+        check_call(tej + ['download', destination, '--id', job_id,
+                          'job1results', '_stdout'],
                    cwd=destdir.path)
         with destdir.open('r', 'job1results') as fp:
             assert fp.read() == 'job output\n'
@@ -131,10 +133,10 @@ def functional_tests():
         destdir.rmtree()
 
     logging.info("List jobs")
-    output = check_output(['tej', 'list', destination])
+    output = check_output(tej + ['list', destination])
     assert output == ('%s finished\n' % job_id).encode('ascii')
 
     logging.info("Remove finished job")
-    check_call(['tej', 'delete', destination, '--id', job_id])
-    output = check_output(['tej', 'status', destination, '--id', job_id])
+    check_call(tej + ['delete', destination, '--id', job_id])
+    output = check_output(tej + ['status', destination, '--id', job_id])
     assert output == b'not found\n'
