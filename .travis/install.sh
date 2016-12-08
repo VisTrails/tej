421511161e4a410daee8f4e93c96a17f5703f811
Remi Rampin <remirampin@gmail.com>
2016-12-07 20:08:02 -0500

    Use released rpaths version

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,6 @@ in
     tests|coverage)
         sudo apt-get update -qq
         sudo apt-get install -qq openssh-client openssh-server
-        pip install 'git+https://github.com/remram44/rpaths.git#egg=rpaths'
         if [ "$TEST_MODE" = coverage ]; then
             pip install coverage codecov
             pip install -e .
